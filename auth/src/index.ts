29c63832d8f64efce8cfeae77746f24c9f9f28d4
Davit Vardanyan <jedavard@gmail.com>
2020-10-05 13:23:22 +0400

    local dev setup ready

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,10 @@ app.use(json());
 
 const port = 3000
 
+app.get('/api/users/ping', (req, res) => {
+    res.status(200).send('Pong')
+})
+
 app.listen(port, () => {
     console.log('Auth service is up on ' + port)
 })
