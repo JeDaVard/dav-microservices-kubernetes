50f057e0b07e724e3679e298bdc02e6d2bad7866
Davit Vardanyan <jedavard@gmail.com>
2020-10-17 18:55:14 +0400

    working on payment service, new publisher for the common srv

--- payment/src/routes/new.ts
+++ payment/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@kuber-ticket/micro-auth'
 import { Order } from '../models/order'
 import { stripe } from '../stripe'
+import { Payment } from '../models/payment'
 
 const router = Router()
 
@@ -30,12 +31,18 @@ router.post(
         if (order.status === OrderStatus.Cancelled)
             throw new BadRequestError('Cannot pay for a cancelled order')
 
-        await stripe.charges.create({
+        const charge = await stripe.charges.create({
             amount: order.price * 100,
             currency: 'usd',
             description: 'Kuber-ticket charge description',
             source: token,
         })
+
+        const payment = Payment.build({
+            orderId,
+            stripeId: charge.id,
+        })
+        await payment.save()
 
         res.status(201).send({ success: true })
     },
