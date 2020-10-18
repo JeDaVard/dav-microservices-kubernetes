f658a57b0fad4b0f4089d96e17e9390928951522
Davit Vardanyan <jedavard@gmail.com>
2020-10-18 19:35:10 +0400

    test fix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,6 @@ import { app } from './app'
 const port = 3000
 
 ;(async function () {
-    console.log('Auth service is starting ...')
     if (!process.env.JWT_SECRET) {
         throw new Error('JWT_SECRET env variable is missing!')
     }
