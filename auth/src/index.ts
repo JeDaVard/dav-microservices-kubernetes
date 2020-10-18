314e4b3ef42482d766f147d4b7128517365f335a
Davit Vardanyan <jedavard@gmail.com>
2020-10-18 17:06:36 +0400

    git workflow test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import { app } from './app'
 const port = 3000
 
 ;(async function () {
-    console.log('Auth service is starting ..')
+    console.log('Auth service is starting ...')
     if (!process.env.JWT_SECRET) {
         throw new Error('JWT_SECRET env variable is missing!')
     }
